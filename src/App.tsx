557f3859439fb3afdaed3d2124cec651e175ef8f
jun-uen0 <hello@junueno.dev>
2022-08-06 22:35:22 +0900

    added components and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import './App.css';
-import MenuBotton from './components/NavBar/MenuBotton';
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import SideBar from './components/SideBar/SideBar'
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles'
 
 function App() {
   return (
     <>
-      <MenuBotton />
+      <MuiThemeProvider theme={theme}>
+        <NavBar />
+        <SideBar />
+      </MuiThemeProvider>
     </>
-  );
+  )
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#091c33'
+    },
+    secondary: {
+      main: '#ffffff'
+    }
+  },
+  typography: {
+    button: {
+      textTransform: 'none'
+    }
+  }
+})
 
 export default App;
