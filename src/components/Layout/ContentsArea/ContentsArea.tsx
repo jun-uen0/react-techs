3dc024f74ae3d4b0185ed69923eaff6558f5e021
jun-uen0 <hello@junueno.dev>
2022-08-07 18:10:10 +0900

    removed unused line

---
+++ src/components/Layout/ContentsArea/ContentsArea.tsx
@@ -0,0 +1,57 @@
+const ContentsArea: React.FC = () => {
+
+  return (
+    <>
+      <p>
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+        ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea / ContentsArea
+      </p>
+    </>
+  )
+}
+
+export default ContentsArea
