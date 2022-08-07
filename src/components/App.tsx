bbb96267b14f8792f0af257afdacf661f149e838
jun-uen0 <hello@junueno.dev>
2022-08-07 18:09:34 +0900

    applied global css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,26 @@
-// import './App.css'
 import NavBar from './Layout/NavBar/NavBar'
 import SideBar from './Layout/SideBar/SideBar'
+import ContentsArea from './Layout/ContentsArea/ContentsArea'
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles'
 
 function App() {
   return (
-    <>
+    <div className="App">
       <MuiThemeProvider theme={theme}>
-        <NavBar />
-        <SideBar />
+        <div className="header">
+          <NavBar />
+        </div>
+        <div className="container">
+          <aside className="drawer">
+            <SideBar />
+          </aside>
+          <main className="main">
+            <ContentsArea />
+          </main>
+        </div>
       </MuiThemeProvider>
-    </>
-  )
+    </div>
+  );
 }
 
 const theme = createTheme({
@@ -31,3 +40,4 @@ const theme = createTheme({
 })
 
 export default App;
+
