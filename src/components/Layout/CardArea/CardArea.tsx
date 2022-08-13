078ca8aef2aef787f83f00650b3283d4e3dc2bf5
jun-uen0 <hello@junueno.dev>
2022-08-13 10:21:56 +0900

    removed unused files

--- src/components/Layout/CardArea/CardArea.tsx
+++ src/components/Layout/CardArea/CardArea.tsx
@@ -1,17 +1,9 @@
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import useTheme from '@material-ui/styles/useTheme'
 import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Typography from '@material-ui/core/Typography'
 import Profile from './parts/Profile'
 
 export default function ClippedDrawer() {
-
-  const classes = useStyles()
-  const theme = useTheme();
 
   return (
     <>
