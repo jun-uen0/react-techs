4da4eabbb3ae37cae90746306c200ab743274f5f
jun-uen0 <hello@junueno.dev>
2022-09-10 14:12:12 +0900

    add fake files

---
+++ src/components/Algorithms/Algorithms.tsx
@@ -0,0 +1,17 @@
+const Algorithms = () => {
+  return (
+    <>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+      <h1>Algorithms</h1>
+    </>
+  )
+}
+
+export default Algorithms
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,18 @@
+const Algorithms = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </>
+  )
+}
+
+export default Algorithms
