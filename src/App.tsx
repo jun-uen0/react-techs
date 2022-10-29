4594655860f9f3e0b1c22a5f7b3877cb55fe6c39
jun-uen0 <hello@junueno.dev>
2022-10-29 15:49:53 +0900

    rm: license

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,32 @@ import {
   Route,
 } from 'react-router-dom';
 
+// input onChange handler
+// return argument with space every 4 digits
+const formatCardNumber = (value: string) => {
+  const cardNumber = value.replace(/\s/g, '')
+  console.log(cardNumber)
+  const cardNumberArray = cardNumber.match(/.{1,4}/g)
+  console.log(cardNumberArray)
+  console.log(cardNumberArray?.join(' '))
+  return cardNumberArray ? cardNumberArray.join(' ') : ''
+}
+
 const App: React.FC = () => {
   return (
     <>
+    <input
+      type="number"
+      onChange={(e) => {
+        console.log(e.target.value)
+      }}
+    >
+    </input>
+    <button>
+      button
+    </button>
+
+
       <Router>
         <Routes>
           <Route path='/' element={< Layout />}></Route>
