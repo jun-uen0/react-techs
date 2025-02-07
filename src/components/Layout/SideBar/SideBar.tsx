f3e70098b8c47512dd6d38b78279b79fb67f41b3
Jun Ueno <hello@junueno.dev>
2025-02-08 00:15:21 +0900

    fix: color of side bar

--- src/components/Layout/SideBar/SideBar.tsx
+++ src/components/Layout/SideBar/SideBar.tsx
@@ -37,7 +37,7 @@ const SideBar: React.FC<SideBarProps> = (props) => {
               keepMounted: true,
             }}
             PaperProps={{
-              style: { backgroundColor: '#303030', color: '#ffffff' },
+              style: { backgroundColor: '#ffffff', color: '#000000' }
             }}
           >
             <Box sx={{ width: 240 }}>
